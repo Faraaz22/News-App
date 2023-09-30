7af2e4f4bb6e956b1447dfff21700f5e615243b3
owaissalauddinkhan <owaissalauddinkhan@gmail.com>
2023-09-30 20:12:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import React,{Component} from 'react';
 
-function App() {
+export default class App extends Component {
+  apiKey = '938ab22941474281a91cc7d4f4168bce';
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+    <Navbar/>
+    <Switch>
+    <Route exact path="/">
+             <News pageSize={6} category='general' apiKey = {this.apiKey} key= "general"/>
+          </Route>
+          <Route exact path="/general">
+             <News pageSize={6} category='general' apiKey = {this.apiKey} key= "general"/>
+          </Route>
+          <Route exact path="/buisness">
+          <News pageSize={6} category='buisness' apiKey = {this.apiKey} key= "buisness"/>
+          </Route>
+          <Route exact path="/health">
+          <News pageSize={6} category='health' apiKey = {this.apiKey} key= "health"/>
+          </Route>
+          <Route exact path="/entertainment">
+          <News pageSize={6} category='entertainment' apiKey = {this.apiKey} key= "entertainment"/>
+          </Route>
+          <Route exact path="/sports">
+          <News pageSize={6} category='sports' apiKey = {this.apiKey} key= "sports"/>
+          </Route>
+          <Route exact path="/technology">
+          <News pageSize={6} category='technology' apiKey = {this.apiKey} key= "technology"/>
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
+  }
 }
 
-export default App;
+
+
+
